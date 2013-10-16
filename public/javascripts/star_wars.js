cdb776af72cdefdfb850cfc9d21064440d9a21de
Brandon <bdawg@robotsnarls.local>
2013-10-15 19:01:23 -0700

    fixed delete key issue, it works now and updated html page

--- public/javascripts/star_wars.js
+++ public/javascripts/star_wars.js
@@ -2,11 +2,21 @@ var typed_stuff = [];
 
 document.onkeypress = function(event){
 	var key_press,key_code;
-	// if the key pressed is anything but shift
-	if (event.keyCode!=16) {
-		key_code = event.keyCode;
-		key_press = String.fromCharCode(event.keyCode);
-		typed_stuff.push(key_press);
+	
+	key_code = event.keyCode;
+	key_press = String.fromCharCode(event.keyCode);
+	typed_stuff.push(key_press);
+	document.getElementById('typed').innerHTML =
+	typed_stuff.join("");
+}
+
+document.onkeydown = function(event) {
+	var key_press, key_code;
+
+	key_code = event.keyCode;
+	key_press = String.fromCharCode(event.keyCode);
+	if(key_code === 8){
+		typed_stuff.pop();
 		document.getElementById('typed').innerHTML =
 		typed_stuff.join("");
 	}
